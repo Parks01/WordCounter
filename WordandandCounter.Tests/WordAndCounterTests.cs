ea4e24f0786655c80462e8efa743ef9088bf8d85
Andrew Parks <parksdrew01@gmail.com>
2017-08-11 12:05:52 -0700

    add test method and class

--- WordandandCounter.Tests/WordAndCounterTests.cs
+++ WordandandCounter.Tests/WordAndCounterTests.cs
@@ -5,3 +5,22 @@ using System;
 
 namespace WordAndCounter.Tests
 {
+  [TestClass]
+  public class WordCounter
+  {
+
+
+    [TestMethod]
+  public Class WordCounterTests_SetMainWord_Void()
+  {
+    //Arrange
+          string expected = "hello world";
+          //Act
+          FindReplace.SetMainArticle("hello world");
+          string actual = FindReplace.GetMainArticle();
+          //Assert
+          Assert.AreEqual(expected, actual);
+
+
+  }
+}
